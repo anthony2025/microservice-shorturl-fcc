0c79fa0caab90cf35484e99ef08a386abd66352b
Anthony Ascencio <anthony2025@gmail.com>
2016-11-12 00:52:52 -0500

    getting rid of unecessary shit, app not working

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-// require dependencies
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
 
-// setting up environment variables
-require('dotenv').config({silent: false})
 const PORT = process.env.PORT || 4000
 const DBURI = process.env.DBURI || 'mongodb://localhost/tododb'
 
-// setting up mongoose
 mongoose.connect(DBURI)
 
-// setting up bodyParser
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// setting up the express router
-const taskRouter = require('./routes/task-router')
-app.use('/api/tasks', taskRouter)
-
-// setting up a homepage
-app.get('/', (req, res) => res.send('Try localhost:' + PORT + '/api/tasks'))
-
-// setting up the express server
+// app.use('/', require('./router'))
+app.get('/', express.static('public'))
 app.listen(PORT, () => console.log('App running on port: ' + PORT))
